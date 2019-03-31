120515ab1c9f5b7796c852087a13ef802dd81260
Sarose550 <43526712+Sarose550@users.noreply.github.com>
2019-03-31 19:42:35 -0400

    Update MyLinkedList.java

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,25 +1,76 @@
-public class Node<E>{
-	private int prev;
-	private int next;
+public class MyLinkedList<E>{
+	private int size;
+	private Node<E> start;
+	private Node<E> end;
+
+
+	public MyLinkedList(){
+		size = 0;
+	}
+
+	public void clear(){
+		size = 0;
+		start = null;
+		end = null;
+	}
+
+	public boolean add(E data){
+		Node<E> n = new Node<E>(data, null);
+		end.setNext(n);
+		this.end = n;
+		size += 1;
+		return true;
+	}
+
+	public void extend(MyLinkedList<E> other){
+		end.setNext(other.start);
+		this.end = other.end;
+		size += other.size;
+		other.clear();
+	}
+
+	public E removeFront(){
+		E data = start.getData();
+		if(size == 1){
+			start = null;
+			size = 0;
+		}
+		else{
+			Node newstart = start.getNext();
+			start = newstart;
+			size -= 1;
+		}
+		return data;
+	}
+}
+
+class Node<E>{
+	private Node next;
 	private E data;
 
-	public Node<E>(){
+	public Node(){
 		data = null;
 		next = null;
-		prev = null;
 	}
 
-	public Node<E>(E data, int prev, int next){
-		data = null;
-		next = null;
-		prev = null;
+	public Node(E stuff, Node n){
+		data = stuff;
+		next = n;
 	}
 
-	public int next(){
+	public Node getNext(){
 		return next;
 	}
 
-	public int prev(){
-		return prev;
+	public E getData(){
+		return data;
+	}
+	
+	public void setNext(Node other){
+		next = other;
+	}
+
+	public void setData(E stuff){
+		data = stuff;
 	}
 }
