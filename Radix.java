b2aa09da95da928f65cbae3750976bc714327057
Sarose550 <43526712+Sarose550@users.noreply.github.com>
2019-04-07 16:30:22 -0400

    Update Radix.java

--- Radix.java
+++ Radix.java
@@ -1,38 +1,37 @@
+import java.util.*;
 public class Radix{
 	public static void radixsort(int[] data){
 		int maxabsval = 0;
-		MyLinkedList<Integer> holder = new MyLinkedList<Integer>();
 		for(int i = 0; i < data.length; i++){
 			if(Math.abs(data[i]) > maxabsval){
 				maxabsval = Math.abs(data[i]);
 			}
-			holder.addLast(data[i]);
-		}
+		}//holder contains all the data in a linked list
 		int a = 1;
 		int numrounds = 0;
 		while(maxabsval >= a){
 			numrounds++;
 			a *= 10;
+		}//this will get the absolute value of the highest number (maxabsval), and the number of digits it has (numrounds)
+		@SuppressWarnings({"rawtypes", "unchecked"})
+		MyLinkedList<Integer>[] buckets = new MyLinkedList[19];//ending in -9 to 9
+		for(int x = 0; x < 19; x++){
+			buckets[x] = new MyLinkedList<Integer>();
 		}
-		MyLinkedList<Integer>[] buckets = new MyLinkedList<Integer>[10];
-	}
-
-	public static void sortBuckets(int[] data, int round, MyLinkedList<Integer>[] buckets, MyLinkedList<Integer> holder){
-		for(int i = 0; i < data.length; i++){
-			if(data[i] >= 0){
-				buckets[getNthDig(data[i], round)].addLast(data[i]);
+		int place = 1;
+		for(int i = 0; i < numrounds; i++){
+			for(int n = 0; n < data.length; n++){
+				//System.out.println(getNthDig(data[n], place));
+				buckets[getNthDig(data[n], place) + 9].addLast(data[n]);
 			}
-			else{
-				buckets[getNthDig(Math.abs(data[i]), round)].addFirst(data[i]);
+			int counter = 0;
+			for(int k = 0; k < 19; k++){
+				while(buckets[k].getSize() > 0){//now we copy from the buckets to the array
+					data[counter] = buckets[k].removeFront();
+					counter++;
+				}
 			}
-		}
-
-		holder.clear();
-		for(int j = 0; j < 10; j++){
-			Node cur = buckets[j].getStart();
-			while(cur != null){
-				if(buckets[j])
-			}
+			place++;
 		}
 	}
 
